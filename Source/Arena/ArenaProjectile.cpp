fc464fcefadea143cf4c4c0f3f8e121f3a87d593
George Yatsunenko <nerotul@gmail.com>
2023-07-04 13:31:34 +0700

    Character mesh and shooting replication added

--- Source/Arena/ArenaProjectile.cpp
+++ Source/Arena/ArenaProjectile.cpp
@@ -6,6 +6,8 @@
 
 AArenaProjectile::AArenaProjectile() 
 {
+	bReplicates = true;
+
 	// Use a sphere as a simple collision representation
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
 	CollisionComp->InitSphereRadius(5.0f);
